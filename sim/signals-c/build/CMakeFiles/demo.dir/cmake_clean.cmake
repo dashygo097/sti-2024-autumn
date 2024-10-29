f3cf203d4bec4b1123a3c79d273c7119f233fcc6
dashygo <dashygo@Zhuanz1.local>
2024-10-29 18:28:38 +0800

    simulations of am and fn are done, second commit

---
+++ sim/signals-c/build/CMakeFiles/demo.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "/Users/dashygo/Dev/Elec/signals-c/demo"
+  "/Users/dashygo/Dev/Elec/signals-c/demo.pdb"
+  "CMakeFiles/demo.dir/src/FO.c.o"
+  "CMakeFiles/demo.dir/src/FO.c.o.d"
+  "CMakeFiles/demo.dir/src/main.c.o"
+  "CMakeFiles/demo.dir/src/main.c.o.d"
+  "CMakeFiles/demo.dir/src/signals.c.o"
+  "CMakeFiles/demo.dir/src/signals.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
